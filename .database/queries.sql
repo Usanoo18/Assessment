eebabf3b91c99afca86e33ac778487ee3996b83b
Usaid <muhammad.noor2@education.nsw.gov.au>
2024-11-27 18:23:44 +1100

    second commit

---
+++ .database/queries.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    user_name TEXT NOT NULL,
+                    email TEXT NOT NULL,
+                    password TEXT NOT NULL,
+                    
