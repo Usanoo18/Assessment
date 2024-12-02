b7315fcbaf636a58c444131a9f4d6c7e84ca7606
Usaid <muhammad.noor2@education.nsw.gov.au>
2024-12-02 22:24:21 +1100

    3rd commit

--- .database/queries.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    user_name TEXT NOT NULL,
-                    email TEXT NOT NULL,
-                    password TEXT NOT NULL,
-                    
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.secret_key = 'goku'
 
 @app.route("/")
 def home():
     return "This is the home page"
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
