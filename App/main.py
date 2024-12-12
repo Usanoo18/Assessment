097e1f9ed898c04ffac4e4cc14fdf683e648cca8
Usaid <muhammad.noor2@education.nsw.gov.au>
2024-12-12 23:10:06 +1100

    addition of html and css

--- App/main.py
+++ App/main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
+import db
 
 app = Flask(__name__)
 app.secret_key = 'goku'
 
 @app.route("/")
-def home():
-    return "This is the home page"
+def Home():
+    return render_template("templates/index.html")
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+
+app.run(debug=True, port=5000)
