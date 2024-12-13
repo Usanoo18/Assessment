7e9b02354382a31671f280e99f61100a2bc9daba
Usaid <muhammad.noor2@education.nsw.gov.au>
2024-12-13 22:19:22 +1100

    addition of login page

--- App/main.py
+++ App/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, session
 import db
 
 app = Flask(__name__)
@@ -6,7 +6,29 @@ app.secret_key = 'goku'
 
 @app.route("/")
 def Home():
-    return render_template("templates/index.html")
+    reviewdata = db.GetReview()
+    return render_template("index.html", reviews=reviewdata)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def Login():
+
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+
+        user = db.CheckLogin(username, password)
+        if user:
+            session['id'] = user['id']
+            session['username'] = username
+            return redirect("/")
+    
+    return render_template("login.html")
+
+def Logout():
+    session.clear()
+    return redirect("/")
+
 
 
 app.run(debug=True, port=5000)
