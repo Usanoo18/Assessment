3a137303bfeffec80a1fae2c1808efb4db340ae7
Usaid <muhammad.noor2@education.nsw.gov.au>
2024-12-14 12:22:56 +1100

    added review and service worker

--- App/db.py
+++ App/db.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 def GetDB():
     
     
-    db = sqlite3.connect(".database/user.db")
+    db = sqlite3.connect("App/.database/user.db")
     db.row_factory = sqlite3.Row
     
     return db
@@ -13,7 +13,9 @@ def GetReview():
     
     
     db = GetDB()
-    reviews = db.execute("SELECT * FROM Reviews").fetchall()
+    reviews = db.execute("""SELECT Reviews.date, Reviews.type, Reviews.name, Reviews.rating, Users.username
+                         FROM Reviews JOIN Users ON Reviews.user_id = Users.id
+                         ORDER BY date DESC""").fetchall()
     db.close()
     return reviews
 
@@ -38,4 +40,13 @@ def RegisterUser(username, password):
     db.execute("INSERT INTO Users(username, password) VALUES(?, ?)", (username, hash,))
     db.commit
 
+    return True
+
+def AddReview(user_id, date, type, rating, review, name):
+    if date is None or type is None or rating is None or review is None or name is None:
+        return False
+    
+    db=GetDB()
+    db.execute("INSERT INTO Reviews(user_id, type, name, rating, review, date ) VALUES (?, ?, ?, ?, ?, ?)", (user_id, type, name, rating, review, date,))
+    db.commit
     return True
--- App/main.py
+++ App/main.py
@@ -12,6 +12,8 @@ def Home():
 
 @app.route("/login", methods=["GET", "POST"])
 def Login():
+    if 'user.id' in session:
+        return redirect("/")
 
     if request.method == "POST":
         username = request.form['username']
@@ -25,10 +27,39 @@ def Login():
     
     return render_template("login.html")
 
+@app.route("/logout")
 def Logout():
     session.clear()
     return redirect("/")
 
+
+@app.route("/register", methods=["GET", "POST"])
+def Register():
+    if 'user.id' in session:
+        return redirect("/")
+
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+
+        if db.RegisterUser(username, password):
+            return redirect("/")
+    return render_template("register.html")
+
+
+@app.route("/add", methods=["GET","POST"])
+def Add():
+    if session.get('username') == None:
+        return redirect("/")
+
+    if request.method == "POST":
+        user_id = session['id']
+        date = request.form['date']
+        type = request.form['type']
+        name = request.form['name']
+        rating = request.form['rating']
+        review = request.form['review']
+    return render_template("add.html")
 
 
 app.run(debug=True, port=5000)
